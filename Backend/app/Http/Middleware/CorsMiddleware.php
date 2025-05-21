9c95bccb55b1c43b30aa7ddc7c261bb3462fa2b5
Silvia <sgf9492@gmail.com>
2025-05-21 20:29:04 +0200

    Finalización documentación, puesta a punto memoria. Primeros pasos dockerización proyecto

--- Backend/app/Http/Middleware/CorsMiddleware.php
+++ Backend/app/Http/Middleware/CorsMiddleware.php
@@ -5,8 +5,35 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
+class CorsMiddleware {
 
-class CorsMiddleware{
+    public function handle(Request $request, Closure $next){
+        \Log::info('Middleware CORS ejecutado');
+
+        /*$allowedOrigins = ['http://localhost:4200'];*/
+        $allowedOrigins = ['*'];
+        $origin = $request->headers->get('Origin');
+
+        $headers = [
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Requested-With, Authorization',
+            'Access-Control-Allow-Credentials' => 'true',
+        ];
+
+        if (in_array($origin, $allowedOrigins)) {
+            $headers['Access-Control-Allow-Origin'] = $origin;
+        }
+
+        if ($request->getMethod() === "OPTIONS") {
+            return response()->json('OK', 200, $headers);
+        }
+
+        return $next($request)->withHeaders($headers);
+    }
+}
+
+
+/*class CorsMiddleware{
 
     public function handle(Request $request, Closure $next){
         \Log::info('Middleware CORS ejecutado');
@@ -23,4 +50,4 @@ class CorsMiddleware{
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
     }
-}
+}*/
